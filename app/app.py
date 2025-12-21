1b117dad9533740173b8e680ef462bb39c9b3b1b
Justin Jennings <jjennings@Mac.lan>
2025-12-21 17:39:11 -0600

    Added lines

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def hello():
     return "Hello world"
+    return "This is a new line"
+    return "Goodbye world"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
